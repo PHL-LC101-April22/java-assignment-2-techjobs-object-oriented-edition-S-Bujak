9c25f2114355e5db2f5a16d6692cc335a8bd189d
S-Bujak <106119256+S-Bujak@users.noreply.github.com>
2022-08-18 19:38:25 -0400

    task 3 complete and passes tests

--- src/main/java/org/launchcode/techjobs/oo/Job.java
+++ src/main/java/org/launchcode/techjobs/oo/Job.java
@@ -16,7 +16,20 @@ public class Job {
     // TODO: Add two constructors - one to initialize a unique ID and a second to initialize the
     //  other five fields. The second constructor should also call the first in order to initialize
     //  the 'id' field.
+public Job() {
+    id = nextId;
+    nextId++;
+}
 
+public Job(String name, Employer employer, Location location, PositionType positionType, CoreCompetency coreCompetency) {
+    this();
+    this.name = name;
+    this.employer = employer;
+    this.location = location;
+    this.positionType = positionType;
+    this.coreCompetency = coreCompetency;
+
+}
     // TODO: Add custom equals and hashCode methods. Consider two Job objects "equal" when their id fields
     //  match.
 
@@ -65,5 +78,18 @@ public class Job {
 
     public void setCoreCompetency(CoreCompetency coreCompetency) {
         this.coreCompetency = coreCompetency;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Job job = (Job) o;
+        return id == job.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
     }
 }
