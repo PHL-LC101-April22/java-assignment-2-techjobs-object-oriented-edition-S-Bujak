027cd22b1a3b68e7ba1ff426e2086b421ccd8a6d
S-Bujak <106119256+S-Bujak@users.noreply.github.com>
2022-08-18 21:19:02 -0400

    task 4 complete

--- src/main/java/org/launchcode/techjobs/oo/test/JobTest.java
+++ src/main/java/org/launchcode/techjobs/oo/test/JobTest.java
@@ -1,7 +1,11 @@
 package org.launchcode.techjobs.oo.test;
 
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
+import org.launchcode.techjobs.oo.*;
+
+import static org.junit.Assert.*;
 
 /**
  * Created by LaunchCode
@@ -9,4 +13,44 @@ import org.junit.runners.JUnit4;
 @RunWith(JUnit4.class)
 public class JobTest {
 
+
+
+    @Test
+    public void testSettingJobId () {
+        Job test_job = new Job();
+        Job another_test_job = new Job();
+        assertNotEquals(test_job, another_test_job);
+
+    }
+
+    @Test
+    public void testJobConstructorSetsAllFields () {
+        Job testJob = new Job("Product tester", new Employer("ACME"), new Location("Desert"), new PositionType("Quality control"), new CoreCompetency("Persistence"));
+        assertTrue(testJob.getName() instanceof String);
+        assertEquals("Product tester", testJob.getName());
+        assertTrue(testJob.getEmployer() instanceof Employer);
+        assertEquals("ACME", testJob.getEmployer());
+        assertTrue(testJob.getLocation() instanceof Location);
+        assertEquals("Desert", testJob.getLocation());
+        assertTrue(testJob.getPositionType() instanceof PositionType);
+        assertEquals("Quality control", testJob.getPositionType());
+        assertTrue(testJob.getCoreCompetency() instanceof CoreCompetency);
+        assertEquals("Persistence", testJob.getCoreCompetency());
+
+    }
+
+    @Test
+    public void testJobsForEquality(){
+       Job job1 = new Job ("Product tester", new Employer("ACME"), new Location("Desert"), new PositionType("Quality control"), new CoreCompetency("Persistence"));
+       Job job2 = new Job("Product tester", new Employer("ACME"), new Location("Desert"), new PositionType("Quality control"), new CoreCompetency("Persistence"));
+       assertFalse(job1.equals(job2));
+    }
 }
+//In JobTest, define a test called testSettingJobId. Do not forget to annotate it with @Test.
+//
+//Create two Job objects using the empty constructor.
+//
+//Use assertNotEquals to verify that the IDs of the two objects are distinct.
+//
+//Run the test using the run configuration that you created above.
+
